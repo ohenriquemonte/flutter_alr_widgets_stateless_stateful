151dd9e7bf76f9daa57c013f7e0902596c79641d
Henrique Montenegro <henrique.montenegro@slcagricola.com.br>
2022-07-06 20:52:43 -0300

    01.09 - Criando nosso projeto

---
+++ android/app/src/main/kotlin/br/com/henriquemontenegro/flutter/alr/flutter_alr_widgets_stateless_stateful/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.henriquemontenegro.flutter.alr.flutter_alr_widgets_stateless_stateful
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
