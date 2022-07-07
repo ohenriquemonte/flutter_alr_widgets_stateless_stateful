3f1a2a9a492b95bf32ea27c6adeb1fb0b0d7cf44
Henrique Montenegro <henrique.montenegro@slcagricola.com.br>
2022-07-06 21:52:05 -0300

    02.04 - Column e Row

--- lib/main.dart
+++ lib/main.dart
@@ -14,18 +14,31 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Stack(
-        alignment: AlignmentDirectional.center,
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(color: Colors.red, width: 100, height: 100),
+              Container(color: Colors.blue, width: 50, height: 50),
+            ],
           ),
-          Container(
-            color: Colors.blue,
-            width: 50,
-            height: 50,
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(color: Colors.blue, width: 100, height: 100),
+              Container(color: Colors.red, width: 50, height: 50),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(color: Colors.cyan, height: 50, width: 50),
+              Container(color: Colors.pinkAccent, height: 50, width: 50),
+              Container(color: Colors.purple, height: 50, width: 50),
+            ],
           ),
         ],
       ),
