57d91b44e95a3498ee36b5a3e262faad2a3a527f
Henrique Montenegro <henrique.montenegro@slcagricola.com.br>
2022-07-06 22:54:00 -0300

    02.08 - Overflow e Árvore de Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,33 +16,57 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(color: Colors.red, width: 100, height: 100),
-              Container(color: Colors.blue, width: 50, height: 50),
-            ],
-          ),
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(color: Colors.blue, width: 100, height: 100),
-              Container(color: Colors.red, width: 50, height: 50),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(color: Colors.cyan, height: 50, width: 50),
-              Container(color: Colors.pinkAccent, height: 50, width: 50),
-              Container(color: Colors.purple, height: 50, width: 50),
-            ],
-          ),
-        ],
+      home: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(color: Colors.red, width: 100, height: 100),
+                Container(color: Colors.blue, width: 50, height: 50),
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(color: Colors.blue, width: 100, height: 100),
+                Container(color: Colors.red, width: 50, height: 50),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(color: Colors.cyan, height: 50, width: 50),
+                Container(color: Colors.pinkAccent, height: 50, width: 50),
+                Container(color: Colors.purple, height: 50, width: 50),
+              ],
+            ),
+            Container(
+              color: Colors.amber,
+              height: 30,
+              width: 30000,
+              child: const Text(
+                'Olá mundo!',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 28,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Você pressionou o botão');
+              },
+              child: const Text(
+                'Aperte o botão!',
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
